e6838f7832f79df0b6e0c9fece17b84885bb081d
Aleksander Bjelk <127252911+AleksanderBjelk@users.noreply.github.com>
2025-08-18 16:32:04 +0200

    second page b project

--- src/test/java/com/example/Training/steps/IkanoSteps.java
+++ src/test/java/com/example/Training/steps/IkanoSteps.java
@@ -3,8 +3,10 @@ package com.example.Training.steps;
 import com.example.Training.driver.DriverManager;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -32,8 +34,46 @@ public class IkanoSteps {
     }
 
     @Then("ska jag se rubriken på Ikano {string}")
-    public void ska_jag_se_rubriken_ikano(String xpath) {
-        WebElement heading = driver.findElement(By.xpath(xpath));
+    public void ska_jag_se_rubriken_ikano(String cssSelector) {
+        WebElement heading = driver.findElement(By.cssSelector(cssSelector));
         assertTrue(heading.isDisplayed());
     }
+
+    @And("ska jag se underrubriken på rubriken {string}")
+    public void skaJagSeUnderrubrikenPåRubriken(String cssSelector) {
+        WebElement subHeading = driver.findElement(By.cssSelector(cssSelector));
+        assertTrue(subHeading.isDisplayed());
+    }
+
+    @And("jag ska se text om privatlån {string}")
+    public void jagSkaSeTextOmPrivatlån(String xpath) throws InterruptedException {
+        WebElement privatText = driver.findElement(By.xpath(xpath));
+        assertTrue(privatText.isDisplayed());
+        Thread.sleep(500);
+    }
+
+    @When("cookies fältet kommer upp ska jag godkänna {string}")
+    public void cookiesFältetKommerUppSkaJagGodkänna(String cssSelector) {
+        WebElement cookie = driver.findElement(By.cssSelector(cssSelector));
+        cookie.click();
+    }
+
+    @When("jag trycker på knappen räkna på privatlån {string}")
+    public void jagTryckerPåKnappenRäknaPåPrivatlån(String cssSelector) {
+        WebElement link = driver.findElement(By.cssSelector(cssSelector));
+        link.click();
+    }
+
+    @And("jag inser att jag tryckte fel så jag backar")
+    public void jagInserAttJagTryckteFelSåJagBackar() throws InterruptedException {
+        driver.navigate().back();
+        Thread.sleep(500);
+    }
+
+    @When("jag trycker på rätt länk {string}")
+    public void jagTryckerPåRättLänk(String xpath) {
+        WebElement link = driver.findElement(By.xpath(xpath));
+        link.click();
+    }
+
 }
--- target/test-classes/com/example/Training/ikanobank.feature
+++ target/test-classes/com/example/Training/ikanobank.feature
@@ -1,5 +1,15 @@
 Feature: Ikano Bank startsida
+
   Scenario: Verifiera att startsidan laddas
     Given jag öppnar startsidan på ikano
-    Then ska jag se rubriken på Ikano "/html/body/section[2]/section[1]/div/div/h1"
+    Then ska jag se rubriken på Ikano ".hero-heading"
+    And ska jag se underrubriken på rubriken ".hero-subheading"
+    And jag ska se text om privatlån "//p[strong[contains(text(), 'Information om effektiv ränta för privatlån:')]]"
+    When cookies fältet kommer upp ska jag godkänna "button#onetrust-accept-btn-handler"
+    When jag trycker på knappen räkna på privatlån "a.rt-ikano-button-accent[href='/lana-pengar/privatlan']"
+    And jag inser att jag tryckte fel så jag backar
+    Then ska jag se rubriken på Ikano ".hero-heading"
+    And ska jag se underrubriken på rubriken ".hero-subheading"
+    And jag ska se text om privatlån "//p[strong[contains(text(), 'Information om effektiv ränta för privatlån:')]]"
+    When jag trycker på rätt länk "//strong[contains(text(),'Bolån')]"
 
